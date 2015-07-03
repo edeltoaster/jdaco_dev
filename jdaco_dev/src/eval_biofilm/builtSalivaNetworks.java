3c77fafd42efe452378813724aafc8ee1faebda4
Thorsten <will.thorsten@gmail.com>
2015-07-03 14:33:03 +0200

    biofilm-project network builder implemented

--- jdaco_dev/src/eval_biofilm/builtSalivaNetworks.java
+++ jdaco_dev/src/eval_biofilm/builtSalivaNetworks.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import java.util.Set;
 
 import framework.ConstructedNetworks;
+import framework.DDIN;
+import framework.DataQuery;
 import framework.NetworkBuilder;
 import framework.PPIN;
 import framework.Utilities;
@@ -14,21 +16,38 @@ public class builtSalivaNetworks {
 	static String project_folder = "/Users/tho/Dropbox/Work/biofilm/";
 	
 	public static void main(String[] args) {
-		
+		System.out.println("run at 03.07.15, PrePPI");
 		Map<String, Set<String>> data_prot_map = new HashMap<String, Set<String>>();
 		
-		for (File file:Utilities.getAllSuffixMatchingFilesInSubfolders(project_folder + "proteins/", "speichel.txt")) {
+		for (File file:Utilities.getAllSuffixMatchingFilesInSubfolders(project_folder + "merged_proteins/", "_merged.txt")) {
 			String filename = file.getName();
 			String sample = filename.split("_")[0];
-			System.out.println(file.getAbsolutePath());
+			data_prot_map.put(sample, Utilities.readEntryFile(file.getAbsolutePath()));
 		}
 		
 		// initialize stuff
-		PPIN ppi = new PPIN("mixed_data/human_ppi.tsv.gz");
+		PPIN org_ppi = new PPIN("mixed_data/human_ppi.tsv.gz");
+		NetworkBuilder builder = new NetworkBuilder(org_ppi);
 		
-		NetworkBuilder builder = new NetworkBuilder(ppi);
+	
+		// versioning output
+		System.out.println("versions:");
+		System.out.println("Ensembl: " + builder.getDB());
+		System.out.println("3did: " + DataQuery.get3didVersion());
+		System.out.println("iPfam: " + DataQuery.getIPfamVersion());
+		// process
+		for (String sample:data_prot_map.keySet()) {
+			System.out.println("Processing " + sample + " ...");
+			ConstructedNetworks constr = builder.constructAssociatedNetworksFromProteinSet(data_prot_map.get(sample));
+			PPIN ppi = constr.getPPIN();
+			DDIN ddi = constr.getDDIN();
+			
+			System.out.println("PPI size: "+ ppi.getSizesStr());
+			
+			ppi.writePPIN(project_folder + "networks/" + sample +"_ppi.tsv");
+			ddi.writeDDIN(project_folder + "networks/" + sample +"_ddi.tsv");
+		}
 		
-		ConstructedNetworks constr = builder.constructAssociatedNetworksFromProteinSet(null);
 	}
 
 }
