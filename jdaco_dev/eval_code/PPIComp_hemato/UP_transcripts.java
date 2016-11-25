461c3073c94f43400f27d97b81f655496bf2db1f
Thorsten Will <will.thorsten@gmail.com>
2016-11-25 17:23:58 +0100

    hemato-rewiring eval additions

---
+++ jdaco_dev/eval_code/PPIComp_hemato/UP_transcripts.java
@@ -0,0 +1,24 @@
+package PPIComp_hemato;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import framework.DataQuery;
+import framework.Utilities;
+
+public class UP_transcripts {
+	
+	public static void main(String[] args) {
+		// study data was build using v83
+		DataQuery.enforceSpecificEnsemblRelease("83");
+		
+		String db = DataQuery.getEnsemblOrganismDatabaseFromName("homo sapiens");
+		System.out.println(db);
+		
+		Set<String> protein_transcripts = new HashSet<>(DataQuery.getGenesTranscriptsProteins(db).stream().map(s->s[1]).collect(Collectors.toList()));
+		System.out.println(protein_transcripts.size());
+		
+		Utilities.writeEntries(protein_transcripts, "/Users/tho/Dropbox/Work/projects/hemato_rewiring/diffnet_analysis/scripts/quantitative/protein_transcripts.txt.gz");
+	}
+}
