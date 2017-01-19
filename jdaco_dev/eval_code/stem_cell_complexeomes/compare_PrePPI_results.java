a4d0368fdf3828194644cb6360972b51abb6e708
Thorsten Will <will.thorsten@gmail.com>
2017-01-19 17:18:31 +0100

    to compare complete/hc PrePPI

---
+++ jdaco_dev/eval_code/stem_cell_complexeomes/compare_PrePPI_results.java
@@ -0,0 +1,34 @@
+package stem_cell_complexeomes;
+
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import framework.DACOResultSet;
+import framework.Utilities;
+
+
+public class compare_PrePPI_results {
+	
+	static String daco_results_folder = "/Users/tho/Dropbox/Work/projects/stem_cell_complexeome/DACO_PrePPIhc_TPMgene/res5/";
+	static String networks_folder = "/Users/tho/Dropbox/Work/projects/stem_cell_complexeome/PrePPIhc_TPMgene_networks/";
+	static Set<String> seed = Utilities.readEntryFile("/Users/tho/git/jdaco_dev/jdaco_dev/mixed_data/hocomoco_human_TFs_v10.txt.gz");
+	
+	public static void main(String[] args) {
+		Map<String, DACOResultSet> data = new HashMap<>();
+		for (File f:Utilities.getAllSuffixMatchingFilesInSubfolders(daco_results_folder, ".csv")) {
+			String sample = f.getName().split("\\.")[0];
+			DACOResultSet dr = new DACOResultSet(f.getAbsolutePath(), seed);
+			data.put(sample, dr);
+		}
+		
+		for (String s1:data.keySet())
+			for (String s2:data.keySet()) {
+				if (s1.compareTo(s2) >= 0) 
+					continue;
+				System.out.println(s1 + " " + s2 + " " + data.get(s1).getComplexSetsSimilarity(data.get(s2)));
+			}
+	}
+}
