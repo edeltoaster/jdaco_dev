7bc2d365b9786fba4497215a1b9c005df138e724
Thorsten Will <will.thorsten@gmail.com>
2017-01-20 15:36:48 +0100

    sample comparison on complete data added

--- jdaco_dev/eval_code/stem_cell_complexeomes/compare_samples_server.java
+++ jdaco_dev/eval_code/stem_cell_complexeomes/compare_samples_server.java
@@ -12,8 +12,8 @@ import framework.Utilities;
 
 public class compare_samples_server {
 	
-	static String daco_results_folder = "/Users/tho/Dropbox/Work/projects/stem_cell_complexeome/DACO_PrePPIhc_TPMgene/res5/";
-	static Set<String> seed = Utilities.readEntryFile("/Users/tho/git/jdaco_dev/jdaco_dev/mixed_data/hocomoco_human_TFs_v10.txt.gz");
+	static String daco_results_folder = "DACO_PrePPI_TPMgene/res5/";
+	static Set<String> seed = Utilities.readEntryFile("hocomoco_human_TFs_v10.txt.gz");
 	
 	public static void main(String[] args) {
 		Map<String, DACOResultSet> data = new HashMap<>();
@@ -27,7 +27,7 @@ public class compare_samples_server {
 			for (String s2:data.keySet()) {
 				if (s1.compareTo(s2) >= 0) 
 					continue;
-				System.out.println(s1 + " " + s2 + " " + data.get(s1).getComplexSetsSimilarity(data.get(s2)));
+				System.out.println(s1 + " vs " + s2 + " : " + data.get(s1).getComplexSetsSimilarity(data.get(s2)));
 			}
 	}
 }
