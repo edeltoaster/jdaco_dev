c9f218a3f3c6f7429055c2df870199d42408e8ff
Thorsten Will <will.thorsten@gmail.com>
2017-07-17 14:43:13 +0200

    some small statistics helper template added

---
+++ jdaco_dev/eval_code/stem_cell_complexeomes/check_complexome_sizes.java
@@ -0,0 +1,29 @@
+package stem_cell_complexeomes;
+
+
+import java.io.File;
+
+import framework.DACOResultSet;
+import framework.Utilities;
+import stem_cell_complexeomes.definitions;
+
+
+public class check_complexome_sizes {
+	
+	public static void main(String[] args) {
+		definitions.printParameters();
+		
+		double n = 0;
+		double total = 0;
+		for (File f:Utilities.getAllSuffixMatchingFilesInSubfolders(definitions.daco_results_folder, ".csv.gz")) {
+			String sample = f.getName().split("\\.")[0];
+			DACOResultSet dr = new DACOResultSet(f.getAbsolutePath(), definitions.seed_file);
+			System.out.println(sample + " : " + dr.getResult().size() + " complexes.");
+			n += dr.getResult().size();
+			total += 1;
+		}
+		
+		System.out.println();
+		System.out.println("average: " + n/total + " per sample.");
+	}
+}
