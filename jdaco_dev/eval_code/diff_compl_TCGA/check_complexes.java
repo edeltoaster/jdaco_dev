48df5c0273b048df60f1145dc19e0d5db258d151
Thorsten Will <will.thorsten@gmail.com>
2017-08-28 11:34:30 +0200

    preparing TCGA analysis

---
+++ jdaco_dev/eval_code/diff_compl_TCGA/check_complexes.java
@@ -0,0 +1,86 @@
+package diff_compl_TCGA;
+
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import framework.DiffComplexDetector;
+import framework.DiffSeedVarDetector;
+import framework.QuantDACOResultSet;
+import framework.Utilities;
+
+
+public class check_complexes {
+	
+	public static void process(String cancer_type) {
+		System.out.println("Processing " +  cancer_type);
+		Map<String, QuantDACOResultSet> group1 = new HashMap<>();
+		Map<String, QuantDACOResultSet> group2 = new HashMap<>();
+		
+		for (File f:Utilities.getAllSuffixMatchingFilesInSubfolders(definitions.daco_results_folder, ".csv.gz")) {
+			String sample = f.getName().split("\\.")[0];
+			QuantDACOResultSet qdr = new QuantDACOResultSet(f.getAbsolutePath(), definitions.seed, definitions.networks_folder + sample + "_major-transcripts.txt.gz");
+			
+			if (!sample.startsWith(cancer_type))
+				continue;
+			
+			if (sample.contains("normal"))
+				group1.put(sample, qdr);
+			else {
+				group2.put(sample, qdr);
+			}
+		}
+		
+		System.out.println(cancer_type + " normal samples : " + group1.size());
+		System.out.println(cancer_type + " tumor samples : " + group2.size());
+		
+		String tfc_out = definitions.diff_tfc_output_folder.substring(0, definitions.diff_tfc_output_folder.length()-1) + "_" + cancer_type + "/";
+		String compl_out = definitions.diff_complex_output_folder.substring(0, definitions.diff_complex_output_folder.length()-1) + "_" + cancer_type + "/";
+		
+		System.out.println();
+		
+		System.out.println("Determining diff. TF combinations ...");
+		DiffSeedVarDetector dsvd = new DiffSeedVarDetector(group1, group2, definitions.qvalue, definitions.parametric, definitions.paired, definitions.check_supersets, definitions.min_variant_fraction, definitions.no_threads);
+		dsvd.diffTFComplAnalysis(tfc_out, definitions.goa, definitions.binding_data, 0.0001, definitions.d_min, definitions.d_max, true, null, null);
+		
+		System.out.println("Determining enriched TFs ...");
+		DiffSeedVarDetector.SPEnrichment tf_enrich = dsvd.calculateSPEnrichment(definitions.qvalue, definitions.SPEnrich_iterations, definitions.SPEnrich_compl_part_threshold);
+		tf_enrich.writeSignificantSeedProteins(tfc_out + "enriched_pos_TFs.txt", tfc_out + "enriched_neg_TFs.txt");
+		
+		System.out.println();
+		
+		System.out.println("Determining diff. complexes ...");
+		DiffComplexDetector dcd = new DiffComplexDetector(group1, group2, definitions.qvalue, definitions.parametric, definitions.paired, definitions.check_supersets, definitions.min_variant_fraction, definitions.no_threads);
+		dcd.diffTFComplAnalysis(compl_out, definitions.goa, definitions.binding_data, 0.0001, definitions.d_min, definitions.d_max, true, null, null);
+		
+		System.out.println("Determining enriched TF combinations ...");
+		DiffComplexDetector.SPCEnrichment tfc_enrich = dcd.calculateSPCEnrichment(definitions.qvalue, definitions.SPEnrich_iterations, definitions.SPEnrich_compl_part_threshold);
+		tfc_enrich.writeSignificantSeedProteinCombinations(compl_out + "enriched_pos_TFCs.txt", compl_out + "enriched_neg_TFCs.txt");
+		
+		System.out.println("Determining enriched TFs ...");
+		DiffComplexDetector.SPEnrichment tf_enrich2 = dcd.calculateSPEnrichment(definitions.qvalue, definitions.SPEnrich_iterations, definitions.SPEnrich_compl_part_threshold);
+		tf_enrich2.writeSignificantSeedProteins(compl_out + "enriched_pos_TFs.txt", compl_out + "enriched_neg_TFs.txt");
+	
+		System.out.println();
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		definitions.printParameters();
+
+		definitions.goa.printTagInformation();
+		
+		System.out.println();
+
+		Map<String, Integer> cancer_types = new HashMap<>();
+		for (File f:Utilities.getAllSuffixMatchingFilesInSubfolders(definitions.daco_results_folder, ".csv.gz")) {
+			String cancer_type = f.getName().split("_")[0];
+			cancer_types.put(cancer_type, cancer_types.getOrDefault(cancer_type, 0) + 1);
+		}
+		
+		for (String cancer_type:cancer_types.keySet())
+			process(cancer_type);
+		
+	}
+}
---
+++ jdaco_dev/eval_code/diff_compl_TCGA/definitions.java
@@ -0,0 +1,65 @@
+package diff_compl_TCGA;
+
+import java.util.Set;
+
+import framework.GOAnnotator;
+import framework.Utilities;
+
+public class definitions {
+	static String daco_results_folder = "res_95_5/";
+	static String networks_folder = "TCGA_networks/";
+	static String diff_complex_output_folder = "diffcompl_results_95_5_-25-25/";
+	static String diff_tfc_output_folder = "difftfc_results_95_5_-25-25/"; // -15-10 should cover most known dimers, up to 25 even more; few may go higher; -25 as 24 is largest motif and overlap should be allowed
+	
+	static double qvalue = 0.05;
+	static boolean parametric = false;
+	static boolean paired = true;
+	static boolean check_supersets = false;
+	static double min_variant_fraction = 0.7;
+	
+	static int no_threads = 48;
+	
+	static String seed_file = "mixed_data/hocomoco_human_TFs_v10.txt.gz";
+	static Set<String> seed = Utilities.readEntryFile(seed_file);
+	
+	static String binding_data = "mixed_data/hocomoco_v10_EPD_v5_2k.txt.gz";
+	static int d_min = -25;
+	static int d_max = 25;
+	
+	static String GOA_def_file = "mixed_data/stem_tags_retrieved.txt.gz";
+	static GOAnnotator goa = new GOAnnotator(GOA_def_file);
+	
+	static int SPEnrich_iterations = 10000;
+	static int SPEnrich_compl_part_threshold = 10;
+	
+	public static void printParameters() {
+		System.out.println("DACO results folder : " + daco_results_folder);
+		System.out.println("networks folder : " + networks_folder);
+		System.out.println("diff compl results output folder : " + diff_complex_output_folder);
+		System.out.println("diff tfc results output folder : " + diff_tfc_output_folder);
+		
+		System.out.println("q-value : " + qvalue);
+		System.out.println("parametric : " + parametric);
+		System.out.println("paired : " + paired);
+		System.out.println("checking supersets : " + check_supersets);
+		System.out.println("min variant fraction : " + min_variant_fraction);
+		
+		System.out.println("no_threads : " + no_threads);
+		
+		System.out.println("seed file : " + seed_file);
+		
+		System.out.println("binding data file : " + binding_data);
+		System.out.println("d_min : " + d_min);
+		System.out.println("d_max : " + d_max);
+		
+		System.out.println("SPE iterations : " + SPEnrich_iterations);
+		System.out.println("SPE compl. part. threshold : " + SPEnrich_compl_part_threshold);
+	}
+	
+	public static void main(String[] args) {
+		// running updates the GO annotations
+		GOAnnotator goa = new GOAnnotator("9606", false, "/Users/tho/git/jdaco_dev/jdaco_dev/mixed_data/stem_tags.txt");
+		goa.writeRetrievedData("/Users/tho/git/jdaco_dev/jdaco_dev/mixed_data/stem_tags_retrieved.txt.gz");
+		goa.printTagInformation();
+	}
+}
