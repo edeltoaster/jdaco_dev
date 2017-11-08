dae74035cbc32f141395199814b7e53b0a7e6c6c
tho <tho@dhcp13-37.bioinformatik.uni-saarland.de>
2017-11-08 09:57:23 +0100

    additions to monocyte-GRN check

--- jdaco_dev/current_eval_code/diff_compl_mono_geu/check_mono_complexes_GRN.java
+++ jdaco_dev/current_eval_code/diff_compl_mono_geu/check_mono_complexes_GRN.java
@@ -1,7 +1,9 @@
 package diff_compl_mono_geu;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import framework.BindingDataHandler;
 import framework.DiffComplexDetector;
@@ -10,18 +12,23 @@ import framework.RegulatoryNetwork;
 public class check_mono_complexes_GRN {
 	static String compl_results = "/Users/tho/Dropbox/Work/projects/diff_complexome/results/diff_results_95_5_-25-25/mono_dcd_compl.txt";
 	static int no_threads = 4;
-	List<String> markers = Arrays.asList("P08637", "O75015", "P08571");
+	static List<String> markers = Arrays.asList("P08637", "O75015", "P08571");
+	static String out_folder = "/Users/tho/Desktop/";
 	
 	public static void main(String[] args) {
 		
 		DiffComplexDetector.SignSortedComplexesResult res = DiffComplexDetector.readSignSortedComplexResult(compl_results);
-		// TODO: refine
+		Set<String> relevant_proteins = new HashSet<>(markers);
+		res.getMemberSeedComb().values().forEach(c -> relevant_proteins.addAll(c));
+		
 		System.out.println("Read binding data ...");
-		BindingDataHandler bdh = new BindingDataHandler(definitions.binding_data);
+		BindingDataHandler bdh = new BindingDataHandler(definitions.binding_data, relevant_proteins, relevant_proteins);
 		
 		System.out.println("Building RegNet ...");
 		RegulatoryNetwork regnet = new RegulatoryNetwork(res.getSignificanceSortedComplexes(), bdh, definitions.d_min, definitions.d_max, no_threads, 1);
 		
 		System.out.println(regnet.getSizesStr());
+		regnet.writeRegulatoryNetwork(out_folder + "regnet.txt", 2);
+		regnet.writeNodeTable(out_folder + "regnet_nodes.txt");
 	}
 }
