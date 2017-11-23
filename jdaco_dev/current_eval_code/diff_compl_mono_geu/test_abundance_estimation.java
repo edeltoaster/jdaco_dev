c0a04970716e5437d8a7f2bd02a0d1179f71acd4
tho <tho@dhcp13-32.bioinformatik.uni-saarland.de>
2017-11-23 12:45:45 +0100

    decreased #iterations in abundance-test

--- jdaco_dev/current_eval_code/diff_compl_mono_geu/test_abundance_estimation.java
+++ jdaco_dev/current_eval_code/diff_compl_mono_geu/test_abundance_estimation.java
@@ -240,7 +240,7 @@ public class test_abundance_estimation {
 		all_iterations.add("sample std prefactor iter corr_compl rmsd_compl corr_rem rmsd_rem");
 		sample_construction.add("sample std prefactor iter complex_abundance_min complex_abundance_mean complex_abundance_std complex_abundance_max lim_distr_min lim_distr_mean lim_distr_std lim_distr_max rem_abundance_min rem_abundance_mean rem_abundance_std rem_abundance_max");
 		
-		int no_iterations = 20;
+		int no_iterations = 10;
 		double[] stds = new double[]{0.1, 0.25, 0.5, 0.75, 1.0};
 		double[] prefactors = new double[]{0.1, 0.25, 0.5, 0.75, 1.0, 1.25, 1.5};
 		
