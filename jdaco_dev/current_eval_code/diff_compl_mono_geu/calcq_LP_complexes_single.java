92cb76c741e96570e24224fa0336c3b4092f980b
tho <tho@wlan-eduroam5-10-9-33-108.funklan.uni-saarland.de>
2018-04-05 13:39:18 +0200

    added single calc variant for calcq_LP

---
+++ jdaco_dev/current_eval_code/diff_compl_mono_geu/calcq_LP_complexes_single.java
@@ -0,0 +1,43 @@
+package diff_compl_mono_geu;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import framework.QuantDACOResultSet;
+import framework.Utilities;
+
+public class calcq_LP_complexes_single {
+
+	public static void main(String[] args) {
+
+		String sample = args[0];
+		QuantDACOResultSet qdr = new QuantDACOResultSet(sample + ".csv.gz", definitions.seed, definitions.networks_folder + sample + "_major-transcripts.txt.gz");
+
+		
+		System.out.println("calculating " + sample);
+		long start = System.currentTimeMillis();
+		LP_algo lp = new LP_algo(qdr);
+		long end = System.currentTimeMillis();
+		
+		long duration_LP = TimeUnit.MILLISECONDS.toSeconds(end - start);
+		
+		start = System.currentTimeMillis();
+		qdr.getAbundanceOfComplexes();
+		end = System.currentTimeMillis();
+		
+		long duration_approx = TimeUnit.MILLISECONDS.toSeconds(end - start);
+		System.out.println(duration_LP + " " + duration_approx);
+		
+		Map<HashSet<String>, Double> abundances = lp.getAbundanceOfComplexes();
+		List<String> to_write = new LinkedList<>();
+		
+		for (HashSet<String> cluster : abundances.keySet())
+			to_write.add( String.join(",", cluster) + " " + abundances.get(cluster));
+		
+		Utilities.writeEntries(to_write, "lp_q_results_95_5/" + sample + ".txt.gz");
+		
+	}
+}
