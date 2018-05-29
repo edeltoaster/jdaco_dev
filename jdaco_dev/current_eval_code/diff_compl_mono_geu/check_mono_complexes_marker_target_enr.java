ed8a0963395620fc5b627de8ab22c0bdfef018d3
tho <tho@dhcp13-44.bioinformatik.uni-saarland.de>
2018-05-29 14:34:21 +0200

    added complex-min_variant-filtering

--- jdaco_dev/current_eval_code/diff_compl_mono_geu/check_mono_complexes_marker_target_enr.java
+++ jdaco_dev/current_eval_code/diff_compl_mono_geu/check_mono_complexes_marker_target_enr.java
@@ -3,8 +3,10 @@ package diff_compl_mono_geu;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import framework.BindingDataHandler;
@@ -36,7 +38,11 @@ public class check_mono_complexes_marker_target_enr {
 		System.out.println("from " + sign_complexes_size + " sign.: " + getNumberOfComplexesTargetingMarkers(sign_complexes.getSignificanceSortedComplexes()));
 		
 		System.out.println("Reading all complexes");
-		Set<HashSet<String>> all_complexes_set = new HashSet<>();
+		int ncms = 0;
+		int cms = 0;
+		Map<HashSet<String>, Integer> count_map_cm = new HashMap<>();
+		Map<HashSet<String>, Integer> count_map_ncm = new HashMap<>();
+		Set<HashSet<String>> unfiltered_complexes = new HashSet<>();
 		for (File f:Utilities.getAllSuffixMatchingFilesInSubfolders(definitions.local_daco_results_folder, ".csv.gz")) {
 			String sample = f.getName().split("\\.")[0];
 			
@@ -44,15 +50,35 @@ public class check_mono_complexes_marker_target_enr {
 				continue;
 			
 			DACOResultSet qdr = new DACOResultSet(f.getAbsolutePath(), definitions.seed);
-			all_complexes_set.addAll(qdr.getResult());
+			unfiltered_complexes.addAll(qdr.getResult());
+			
+			if (sample.startsWith("non")) {
+				qdr.getResult().stream().forEach(c -> count_map_ncm.put(c, count_map_ncm.getOrDefault(c, 0) + 1));
+				ncms++;
+			}
+			else {
+				qdr.getResult().stream().forEach(c -> count_map_cm.put(c, count_map_cm.getOrDefault(c, 0) + 1));
+				cms++;
+			}
+			
 		}
 		
-		List<HashSet<String>> all_complexes = new ArrayList<HashSet<String>>(all_complexes_set);
+		List<HashSet<String>> all_complexes = new ArrayList<HashSet<String>>(unfiltered_complexes);
+		System.out.println("pre-pruning: " + all_complexes.size());
+		double min_count_cms = definitions.min_variant_fraction * cms;
+		double min_count_ncms = definitions.min_variant_fraction * ncms;
+		for (HashSet<String> complex:unfiltered_complexes) {
+			// filter
+			if (count_map_ncm.getOrDefault(complex, 0) < min_count_ncms && count_map_cm.getOrDefault(complex, 0) < min_count_cms)
+				all_complexes.remove(complex);
+		}
+		System.out.println("post-pruning: " + all_complexes.size());
 		
 		for (int i = 0; i < no_iterations; i++) {
 			Collections.shuffle(all_complexes);
 			List<HashSet<String>> test_complexes = new ArrayList<>(all_complexes.subList(0, sign_complexes_size));
-			System.out.println("from " + test_complexes.size() + " test.: " + getNumberOfComplexesTargetingMarkers(test_complexes));
+			int hits = getNumberOfComplexesTargetingMarkers(test_complexes);
+			System.out.println("from " + test_complexes.size() + " test.: " + hits);
 		}
 		
 	}
