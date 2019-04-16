6412263adee34942a28d93f992b8aa03d51e1782
tho <tho@dhcp13-44.bioinformatik.uni-saarland.de>
2019-04-16 16:35:04 +0200

    complex quantification added to breastcancer prot proj

---
+++ jdaco_dev/current_eval_code/breastcancer_proteome_complexome/diff_complexes_mentha.java
@@ -0,0 +1,36 @@
+package breastcancer_proteome_complexome;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import framework.QuantDACOResultSet;
+import framework.Utilities;
+
+public class diff_complexes_mentha {
+	static String expr_folder = "/Users/tho/GDrive/Work/projects/breastcancer_proteome_complexomes/abundances/";
+	static String net_folder = "/Users/tho/GDrive/Work/projects/breastcancer_proteome_complexomes/mentha_networks/";
+	static String compl_folder = "/Users/tho/GDrive/Work/projects/breastcancer_proteome_complexomes/res/";
+	static String seed_file = "/Users/tho/GDrive/Work/projects/breastcancer_proteome_complexomes/JDACO_run_stuff/hocomoco_human_core_TFs_v11.txt.gz";
+	static String qr_folder = "/Users/tho/GDrive/Work/projects/breastcancer_proteome_complexomes/quant_compl/";
+	
+	static Map<String, QuantDACOResultSet> data = new HashMap<>();
+	
+	public static void load_data(boolean write) {
+		
+		for (File f:Utilities.getAllSuffixMatchingFilesInSubfolders(compl_folder, ".csv.gz")) {
+			String sample = f.getName().split("\\.")[0];
+			System.out.println("Processing " + sample);
+			QuantDACOResultSet qdr = new QuantDACOResultSet(f.getAbsolutePath(), seed_file, net_folder + sample + "_major-transcripts.txt.gz");
+			
+			if (write)
+				qdr.writeQuantifiedResult(qr_folder + sample + ".txt.gz");
+			
+			data.put(sample, qdr);
+		}
+	}
+	
+	public static void main(String[] args) {
+		load_data(true);
+	}
+}
