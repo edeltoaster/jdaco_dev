187c614ae8162fbb0be6ad0c8cd59043cc3fc1d5
tho <tho@dhcp13-44.bioinformatik.uni-saarland.de>
2019-09-27 11:18:58 +0200

    slight changes in KDEL project data prep

--- jdaco_dev/current_eval_code/KDEL/retrieve_annotation_data.java
+++ jdaco_dev/current_eval_code/KDEL/retrieve_annotation_data.java
@@ -12,10 +12,11 @@ import framework.Utilities;
 public class retrieve_annotation_data {
 
 	static int min_annotated_genes = 4;
+	static List<String> go_terms = new LinkedList<>();
 	
 	public static void writeOut(String file, Map<String, Set<String>> data) {
 		List<String> lines = new LinkedList<String>();
-		for (String term:data.keySet()) {
+		for (String term : go_terms) {
 			if (data.get(term).size() < min_annotated_genes)
 				continue;
 			String temp = term + "\t"+ String.join(",", data.get(term));
@@ -27,19 +28,19 @@ public class retrieve_annotation_data {
 		// read Björn's GO terms of interest
 		
 		Map<String, String> go_tois = new HashMap<>();
-		for (String s : Utilities.readEntryFile("/Users/tho/GDrive/Work/projects/KDEL/new_stuff/enrichment/GO_TOI.csv")) {
+		for (String s : Utilities.readFile("/Users/tho/GDrive/Work/projects/KDEL/new_stuff/enrichment/GO_TOI.csv")) {
 			if (!s.contains(":"))
 				continue;
 			String[] spl = s.trim().split(";");
 			go_tois.put(spl[0], spl[2]);
+			go_terms.add(spl[2]);
 		}
-		
 		
 		Map<String, Set<String>> annotation_map = new HashMap<String, Set<String>>();
 		String human = "9606";
 		String out_file = "/Users/tho/Desktop/annotation_data.tsv";
 		
-		for (String go_term : go_tois.keySet()) {
+		for (String go_term : go_tois.keySet() ) {
 			String go_descr = go_tois.get(go_term);
 			Set<String> temp = DataQuery.getGenesWithGO(go_term, human);
 			System.out.println(go_descr + " : " + temp.size());
@@ -49,10 +50,11 @@ public class retrieve_annotation_data {
 		writeOut(out_file, annotation_map);
 		
 		
+		System.out.println("Only experimental data:");
 		annotation_map.clear();
 		out_file = "/Users/tho/Desktop/annotation_data_exp.tsv";
 		
-		for (String go_term : go_tois.keySet()) {
+		for (String go_term : go_tois.keySet() ) {
 			String go_descr = go_tois.get(go_term);
 			Set<String> temp = DataQuery.getGenesWithGO(go_term, human, false, true);
 			System.out.println(go_descr + " : " + temp.size());
